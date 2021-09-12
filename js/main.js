021dc800c0af0dba394d87feaaffa9b360e7036c
Dylan Suarez <dylssuarez@misena.edu.co>
2021-09-11 19:09:22 -0500

    sidebar collapse animation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ const d = document;
 
 
 import {menuHamburger} from "./menu.js";
+import {sidebarCollapse} from "./sidebar.js"
 
 d.addEventListener('DOMContentLoaded', (e) => {
   
-  menuHamburger(".svg", ".menu");
+  menuHamburger(".btn_menu svg", ".btn_menu path", ".menu");
+  sidebarCollapse(".btn-sidebar img");
 
 });
--- js/menu.js
+++ js/menu.js
@@ -3,15 +3,14 @@ const d = document;
 
 
 
-export const menuHamburger = function (btn, menuHam) {
+export const menuHamburger = function (btnSvg, btnPath, menuHam) {
   
+  const menu = d.querySelector(menuHam);
   
   d.addEventListener("click", e => {
-    const menu = d.querySelector(menuHam);
-    if (e.target.matches(btn)) {
+    if (e.target.matches(btnSvg) || e.target.matches(btnPath)) {
       menu.classList.toggle("hamburger");
     };
   });
 
-  
 };
