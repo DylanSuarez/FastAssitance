db3048aac79b619e0853c2d34a53e7e9a5f390af
Dylan Suarez <dylssuarez@misena.edu.co>
2021-09-13 11:28:25 -0500

    cambiando el home por grid css

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,15 +4,11 @@ const d = document;
 
 export const sidebarCollapse = function (btnSidebar) {
   
-  const contenido = d.querySelector(".contenido_container");
-  const header = d.querySelector(".header-home");
-  const sidebar = d.querySelector(".sidebar");
+  const home = d.querySelector(".sidebar");
   
   d.addEventListener("click", e => {
     if (e.target.matches(btnSidebar)) {
-      sidebar.classList.toggle("sidebar-collapse");
-      header.classList.toggle("header-home-collapse");
-      contenido.classList.toggle("contenido_container-collapse");
+      home.classList.toggle("sidebar-collapse");
     };
   });
 
